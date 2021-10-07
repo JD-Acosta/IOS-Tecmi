48fb8560f5c46de4181ea55123a1f79bab973c69
JD-Acosta <89108754+JD-Acosta@users.noreply.github.com>
2021-10-06 20:30:19 -0500

    Add files via upload
    Actividad 8. Prototipo

---
+++ Prototipado/Prototipado/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prototipado
+//
+//  Created by user190611 on 10/6/21.
+//  Copyright © 2021 user190611. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
